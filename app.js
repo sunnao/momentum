df71c7ab69c6f6d90a192a413fef2fad54f2df08
sunnao <osa125125@naver.com>
2022-09-08 13:11:08 +0900

    테스트
    테스트입니다

---
+++ app.js
@@ -0,0 +1,14 @@
+const calculator = {
+    add: function(a, b){
+        console.log(a + b);
+    },
+    min: function(a, b){
+        console.log(a - b);
+    },
+    div: function(a, b){
+        console.log(a / b);
+    },
+    powerof: function(a, b){
+        console.log(a ** b);
+    },
+};
