7ec0cf44eb0fecde71df4f57c380c7842f02955c
sunnao <osa125125@naver.com>
2022-09-09 02:56:38 +0900

    현재시간 추가

--- js/clock.js
+++ js/clock.js
@@ -1,3 +1,13 @@
 const clock = document.querySelector("#clock");
 
-clock.innerText = "lalalal";
+function getClock() {
+    const hours = String(new Date().getHours()).padStart(2,"0");
+    const minutes = String(new Date().getMinutes()).padStart(2,"0");
+    const seconds = String(new Date().getSeconds()).padStart(2,"0");
+
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+// 1초 후에 시계 나타나는것 방지. 바로 현재시간 호출
+getClock()
+setInterval(getClock, 1000);
