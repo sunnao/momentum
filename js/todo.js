d11dd2dc826af229456ab630d6e8fee56d0c232a
sunnao <osa125125@naver.com>
2022-09-10 22:42:03 +0900

    ToDo리스트 삭제 반영하기

--- js/todo.js
+++ js/todo.js
@@ -23,12 +23,19 @@ function deleteToDo(event){
     // 여러개 버튼중에서 어떤 버튼 눌렀는지
     const deleteList = event.target.parentElement;
     deleteList.remove();
+
+    // toDos.배열에서 살제할것 없앤걸로 만들어진 배열을 다시 toDos로 재선언
+    // toDo.id는 number타입인데 deleteList.id는 string임. 타입 통일필요
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(deleteList.id));
+    saveToDos();
 };
 
 function paintTodo(newToDo){
     const list = document.createElement("li");
+    list.id = newToDo.id;
     const span = document.createElement("span");
-    span.innerText = newToDo;
+    // 받은거에서 text값을 표시
+    span.innerText = newToDo.text;
     
     const button = document.createElement("button");
     button.innerText = "X";
@@ -46,11 +53,16 @@ function handleToDoSubmit(event) {
     const newToDo = toDoInput.value;
     // localStorage.setItem("toDoliist", newToDo);  --> 될줄 알았지만 덮어씌우는 방법으로 1개만 저장됨
         // 그래서 배열에 일단 저장하고 그 배열을 스토리지에 저장하는 함수 필요 
-    toDos.push(newToDo);
+    
+    const newTodoObj = {
+        id: Date.now(),
+        text: newToDo,
+    }
+    toDos.push(newTodoObj);
     saveToDos(newToDo);
     toDoInput.value = "";   // 이 상황에서의 value값만 없앤것. 
         // 없앤 값을 newToDo변수에 넣지는 않았으므로 newToDo에는 이전 값이 그대로 들어있음.
-    paintTodo(newToDo);
+    paintTodo(newTodoObj);
 };
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
@@ -66,5 +78,5 @@ if(savedToDos) {
     toDos = parsedToDos;
     //화살표 함수. 기존처럼 함수를 따로 설정해도 같은 효과.
     parsedToDos.forEach(paintTodo);
-    // 이제야 새로고침해도 리스트 유지됨
+    // 이제야 새로고침해도 리스트 유지됨.
 }
